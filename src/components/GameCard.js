f30d747984fd4d756667ce7d7e725a07a9622f27
liannaccone <lciannaccone@gmail.com>
2018-03-22 21:49:30 -0700

    removing test code

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -5,7 +5,6 @@ import React from "react";
 const GameCard = props => (
 	<div style={{margin: 15}} onClick={() => props.userClick(props.id)} className="img-container col-md-3" key={props.key}>
 		<img src={props.image} alt={props.name} height="240" width="300" className="img-rounded"/>
-		{props.hasBeenClicked && (<div>heyooooo</div>)}
 	</div>
 );
 
