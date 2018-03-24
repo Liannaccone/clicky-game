461288212e07aaaf723e1b20df11a2466e1c771e
liannaccone <lciannaccone@gmail.com>
2018-03-24 12:02:42 -0700

    adjusting size of images

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const GameCard = props => (
 	<div style={{margin: 15}} onClick={() => props.userClick(props.id)} className="img-container col-md-3" key={props.key}>
-		<img src={props.image} alt={props.name} height="240" width="300" className="img-rounded"/>
+		<img src={props.image} alt={props.name} height="200" width="260" className="img-rounded"/>
 	</div>
 );
 
