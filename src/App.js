1c8440cf3f5b7ba40154d57f8e4c82754a6c77b2
liannaccone <lciannaccone@gmail.com>
2018-03-26 15:02:28 -0700

    updating homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import GameContainer from './components/GameContainer';
-import './App.css';
 
 class App extends Component {
   render() {
